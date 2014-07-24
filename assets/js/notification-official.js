a61b359fd1c13cf949270711feb790b13a4c6560
pgct1 <t1gemini@yandex.ru>
2014-07-25 01:12:59 +0400

    finishing up with official sources

--- assets/js/notification-official.js
+++ assets/js/notification-official.js
@@ -18,6 +18,12 @@
 
 			$scope.article.parsed = true;
 
+			// make the logo url to our stored logo
+
+			$scope.article.sourceLogo = 'images/source-icons/' + $scope.article.Source.replace(/\s/gm, '') + '.png';
+
+			// remove html and trim article
+
 			var clip = $scope.article.Content.Description.replace(/<[^>]+>/gm, '');
 
 			if(clip.length > 200){
@@ -27,6 +33,21 @@
 			}
 
 			$scope.article.Content.Description = clip;
+
+			// parse special characters in the title
+
+			$scope.article.Content.Title = $scope.article.Content.Title.replace(/\&\#39;/g,'\'');
+
+			// construct the link to the article
+
+			if($scope.article.Content.Guid && $scope.article.Content.Guid.substring(0,4) == 'http'){
+				$scope.article.Link = $scope.article.Content.Guid
+			}else if($scope.article.Content.Links.length > 0){
+				$scope.article.Link = $scope.article.Content.Links[0].Href;
+			}else{
+				//wtf?
+				$scope.article.Link = '#';
+			}
 
 		}];
 
